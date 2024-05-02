b54878a3284c688f8b51e5ea60be9a4156f7a0c8
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2024-05-02 15:08:22 +0900

    test

--- cloud_project/urls.py
+++ cloud_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from cloud import views
 from cloud.views import *
 
+#
 urlpatterns = [
     path('', cloud_view, name='home'),  # 루트 URL에 대한 패턴 추가
     path('admin/', admin.site.urls),
